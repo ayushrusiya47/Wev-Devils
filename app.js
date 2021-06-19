1f595f0713473a7c3aef4842a0a9807de3278f66
Shikhar <75368010+shikhar13012001@users.noreply.github.com>
2021-06-19 18:32:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+"use strict"
+const body=document.querySelector('body');
+console.log(body);
+window.addEventListener("resize",resize_blur);
+function resize_blur()
+{
+    body.style.filter="blur(4px)";
+    setTimeout(restore,500);
+  
+}
+function restore(){
+    body.style.filter="blur(0px)";
+}
+const sign_up=document.querySelector(".sign-up");
+sign_up.addEventListener("click",()=>{
+    location.href="./Signup/signup.html"
+})
+const sign_inbtn=document.querySelector(".sign-in");
+
+sign_inbtn.addEventListener("click",function(){
+    location.href="./SignIn/signIn.html"
+});
+ const user=document.querySelector(".user-profile");
+ user.addEventListener("click",()=>{
+     location.href="./user_profile/userprofile.html";
+ })
+
+ setTimeout(() => {
+    document.body.style.backgroundImage="none";
+   
+ }, 2500); 
+ 
+
+ 
