cbac2479a7e08628dbb730cea49b7a0fec2ed74d
Sanket2055 <sanketdiwate95@gmail.com>
2021-06-20 20:56:15 +0530

    token verification done and .some issue in sign-up  linking

--- Signup/signup_app.js
+++ Signup/signup_app.js
@@ -1,13 +1,50 @@
-"use strict"
-const body=document.querySelector('body');
+// "use strict"
+const body = document.querySelector('body');
 console.log(body);
-window.addEventListener("resize",resize_blur);
-function resize_blur()
-{
-    body.style.filter="blur(4px)";
-    setTimeout(restore,200);
-  
+const Url = "http://localhost:8000"
+const signUpForm = document.querySelector(".form");
+window.addEventListener("resize", resize_blur);
+function resize_blur() {
+    body.style.filter = "blur(4px)";
+    setTimeout(restore, 200);
+
 }
-function restore(){
-    body.style.filter="blur(0px)";
+function restore() {
+    body.style.filter = "blur(0px)";
 }
+// facing a issue here
+// signUpForm.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const email = document.querySelector('#email').value
+//     const name = document.querySelector('#username').value
+//     const password = document.querySelector('#password').value
+//     const confirmPassword = document.querySelector('#confirm-password').value
+//     const checkbox = document.querySelector('#check-box').value
+//     console.log(email, name, password);
+//     if (checkbox === false) {
+//         alert("please fill the checkbox");
+
+//     }
+//     if (password !== confirmPassword) {
+//         alert("password is not matching");
+//         return;
+//     }
+//     fetch('${Url}/auth/signup', {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': "application/json",
+
+//         },
+//         body: JSON.stringify({ name, email, password }),
+
+//     }).then((res) => res.json())
+//         .then((data) => {
+//             const { token } = data;
+//             if (token) {
+//                 localStorage.setItem("jwt", token);
+//             } else {
+//                 alert("sign Again");
+//             }
+//         })
+// });
+
--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 "use strict"
-const body=document.querySelector('body');
+const body = document.querySelector('body');
+const Url = "http://localhost:8000"
 console.log(body);
-window.addEventListener("resize",resize_blur);
-function resize_blur()
-{
-    body.style.filter="blur(4px)";
-    setTimeout(restore,500);
-  
+window.addEventListener("resize", resize_blur);
+function resize_blur() {
+    body.style.filter = "blur(4px)";
+    setTimeout(restore, 500);
+
 }
-function restore(){
-    body.style.filter="blur(0px)";
+function restore() {
+    body.style.filter = "blur(0px)";
 }
-const sign_up=document.querySelector(".sign-up");
-sign_up.addEventListener("click",()=>{
-    location.href="./Signup/signup.html"
+const sign_up = document.querySelector(".sign-up");
+sign_up.addEventListener("click", () => {
+    location.href = "./Signup/signup.html"
 })
-const sign_inbtn=document.querySelector(".sign-in");
+const sign_inbtn = document.querySelector(".sign-in");
 
-sign_inbtn.addEventListener("click",function(){
-    location.href="./SignIn/signIn.html"
+sign_inbtn.addEventListener("click", function () {
+    location.href = "./SignIn/signIn.html"
 });
- const user=document.querySelector(".user-profile");
- user.addEventListener("click",()=>{
-     location.href="./user_profile/userprofile.html";
- })
+const user = document.querySelector(".user-profile");
+user.addEventListener("click", () => {
+    location.href = "./user_profile/userprofile.html";
+})
 
- setTimeout(() => {
-    document.body.style.backgroundImage="none";
-   
- }, 2500); 
- 
+setTimeout(() => {
+    document.body.style.backgroundImage = "none";
 
- 
+}, 2500);
