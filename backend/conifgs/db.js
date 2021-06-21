b935925abad080aefc128684dd0bcd82acd05f1b
Ayush Rusiya <76245068+ayushrusiya47@users.noreply.github.com>
2021-06-21 14:10:58 +0530

    Delete backend directory

--- backend/conifgs/db.js
+++
@@ -1,4 +0,0 @@
-const { Client } = require('pg')
-
-const client = new Client(`postgres://fhpcrzhr:ukB1mYKGS1SVX2kBbBVYtHALnKbtIzlp@john.db.elephantsql.com/fhpcrzhr`);
-module.exports = client;
--- backend/routes/auth.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { signUp, signIn } = require('../contollers/auth');
-const router = express.Router();
-router.post("/signup", signUp);
-router.post("/signin", signIn);
-module.exports = router;
