7cc48f85d43bdcb071948d4d257a73dc2c281f88
Sanket2055 <sanketdiwate95@gmail.com>
2021-06-22 17:27:20 +0530

    signin/signup done

--- SignIn/sign_in_app.js
+++ SignIn/sign_in_app.js
@@ -1,13 +1,41 @@
 "use strict"
-const body=document.querySelector('body');
+// const url = "http://localhost:8000"
+const body = document.querySelector('body');
 console.log(body);
-window.addEventListener("resize",resize_blur);
-function resize_blur()
-{
-    body.style.filter="blur(4px)";
-    setTimeout(restore,200);
-  
+window.addEventListener("resize", resize_blur);
+function resize_blur() {
+    body.style.filter = "blur(4px)";
+    setTimeout(restore, 200);
+
 }
-function restore(){
-    body.style.filter="blur(0px)";
+function restore() {
+    body.style.filter = "blur(0px)";
 }
+const signIn = document.querySelector(".form");
+
+signIn.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    fetch('http://localhost:8000/auth/signin', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+
+        },
+        body: JSON.stringify({ email, password }),
+
+    }).then((res) => res.json())
+        .then((data) => {
+            const { token } = data;
+            if (token) {
+                console.log("signed in succesfully");
+                console.log("ok token set")
+                localStorage.setItem("jwt", token);
+                location.href = "../index.html";
+            } else {
+                alert("facing some error , might the user already exists");
+            }
+        })
+});
--- Signup/signup_app.js
+++ Signup/signup_app.js
@@ -12,39 +12,43 @@ function resize_blur() {
 function restore() {
     body.style.filter = "blur(0px)";
 }
-// facing a issue here
-// signUpForm.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     const email = document.querySelector('#email').value
-//     const name = document.querySelector('#username').value
-//     const password = document.querySelector('#password').value
-//     const confirmPassword = document.querySelector('#confirm-password').value
-//     const checkbox = document.querySelector('#check-box').value
-//     console.log(email, name, password);
-//     if (checkbox === false) {
-//         alert("please fill the checkbox");
+signUpForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const email = document.querySelector('#email').value
+    const name = document.querySelector('#username').value
+    const password = document.querySelector('#password').value
+    const confirmPassword = document.querySelector('#confirm-password').value
+    const checkbox = document.querySelector('#check-box').value
+    // console.log(email, name, password);
+    if (checkbox === false) {
+        alert("please fill the checkbox");
 
-//     }
-//     if (password !== confirmPassword) {
-//         alert("password is not matching");
-//         return;
-//     }
-//     fetch('${Url}/auth/signup', {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': "application/json",
+    }
+    if (password !== confirmPassword) {
+        alert("password is not matching");
+        return;
+    }
+    // have to change this after deploying backend
+    fetch("http://localhost:8000/auth/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
 
-//         },
-//         body: JSON.stringify({ name, email, password }),
+        },
+        body: JSON.stringify({ name, email, password }),
 
-//     }).then((res) => res.json())
-//         .then((data) => {
-//             const { token } = data;
-//             if (token) {
-//                 localStorage.setItem("jwt", token);
-//             } else {
-//                 alert("sign Again");
-//             }
-//         })
-// });
+    }).then((res) => res.json())
+        .then((data) => {
+            const { token } = data;
+            if (token) {
+                // setting the token in web local storage
+                localStorage.setItem("jwt", token);
+            } else {
+                alert("facing error please try agian");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
