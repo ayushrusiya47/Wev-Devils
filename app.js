4fcd938de6d89501f9664eaca80c1d90a8f6f4a7
Shikhar <75368010+shikhar13012001@users.noreply.github.com>
2021-06-29 14:39:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
 "use strict"
-const body = document.querySelector('body');
-const Url = "http://localhost:8000"
+const body=document.querySelector('body');
 console.log(body);
-window.addEventListener("resize", resize_blur);
-function resize_blur() {
-    body.style.filter = "blur(4px)";
-    setTimeout(restore, 500);
-
+// const Url = "http://localhost:8000"
+window.addEventListener("resize",resize_blur);
+function resize_blur()
+{
+    body.style.filter="blur(4px)";
+    setTimeout(restore,500);
+  
 }
-function restore() {
-    body.style.filter = "blur(0px)";
+function restore(){
+    body.style.filter="blur(0px)";
 }
-const sign_up = document.querySelector(".sign-up");
-sign_up.addEventListener("click", () => {
-    location.href = "./Signup/signup.html"
+const sign_up=document.querySelector(".sign-up");
+sign_up.addEventListener("click",()=>{
+    location.href="./Signup/signup.html"
 })
-const sign_inbtn = document.querySelector(".sign-in");
-
-sign_inbtn.addEventListener("click", function () {
-    location.href = "./SignIn/signIn.html"
+document.querySelector(".signup").addEventListener("click",()=>{
+    location.href="./Signup/signup.html"
+})
+ 
+const sign_inbtn=document.querySelector(".sign-in");
+ 
+document.querySelector(".signin").addEventListener("click",function(){
+    location.href="./SignIn/signIn.html"
 });
-const user = document.querySelector(".user-profile");
-user.addEventListener("click", () => {
-    location.href = "./user_profile/userprofile.html";
+sign_inbtn.addEventListener("click",function(){
+    location.href="./SignIn/signIn.html"
+});
+ 
+ const user=document.querySelector(".user-profile");
+  
+ user.addEventListener("click",()=>{
+     location.href="./user_profile/userprofile.html";
+ })
+ document.querySelector(".profile").addEventListener("click",()=>{
+    location.href="./user_profile/userprofile.html";
 })
+ const url=`https://cdn.dribbble.com/users/1516344/screenshots/6617658/17_liquidbackgrounds_2_1600x1200_1_4x.jpg`
+ setTimeout(() => {
+    document.body.style.backgroundImage=`url(${url})`;
+   
+ }, 3500); 
+ 
+document.querySelector(".menu").addEventListener("click",()=>{
+    document.querySelector(".overlay").style.display="none";
+});
+ const overlay=document.querySelector(".overlay");
+ document.querySelector(".dropdown").addEventListener("click",()=>{
+     overlay.style.display="block";
+    
+ });
 
-setTimeout(() => {
-    document.body.style.backgroundImage = "none";
-
-}, 2500);
