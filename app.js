7290618d7e4761ba23df4fec7dcf235d8d5a5f0d
Shikhar <75368010+shikhar13012001@users.noreply.github.com>
2021-07-05 13:07:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,45 +28,47 @@ function resize_blur() {
 function restore() {
   body.style.filter = "blur(0px)";
 }
-const sign_up = document.querySelector(".sign-up");
-sign_up.addEventListener("click", () => {
-  location.href = "./Signup/signup.html";
+const sign_up=document.querySelector(".sign-up");
+sign_up.addEventListener("click",()=>{
+    location.href="./Signup/signup.html"
+})
+document.querySelector(".signup").addEventListener("click",()=>{
+    location.href="./Signup/signup.html"
 });
-document.querySelector(".signup").addEventListener("click", () => {
-  location.href = "./Signup/signup.html";
+document.querySelector(".btn-events").addEventListener("click",()=>{location.href="./Events/events.html"})
+const sign_inbtn=document.querySelector(".sign-in");
+ 
+document.querySelector(".signin").addEventListener("click",function(){
+    location.href="./SignIn/signIn.html"
 });
+sign_inbtn.addEventListener("click",function(){
+    location.href="./SignIn/signIn.html"
+});
+ 
+ const user=document.querySelector(".user-profile");
+  
+ user.addEventListener("click",()=>{
+     location.href="./user_profile/userprofile.html";
+ })
+ document.querySelector(".profile").addEventListener("click",()=>{
+    location.href="./user_profile/userprofile.html";
+})
+document.querySelector(".event").addEventListener("click",()=>{
+    location.href="./Events/events.html";
+})
+//  const url=`https://cdn.dribbble.com/users/1516344/screenshots/6617658/17_liquidbackgrounds_2_1600x1200_1_4x.jpg`
+//  setTimeout(() => {
+//     document.body.style.backgroundImage=`url(${url})`;
+   
+//  }, 3500); 
+ 
+document.querySelector(".menu").addEventListener("click",()=>{
+    document.querySelector(".overlay").style.display="none";
+});
+ const overlay=document.querySelector(".overlay");
+ document.querySelector(".dropdown").addEventListener("click",()=>{
+     overlay.style.display="block";
+    
+ });
 
-const sign_inbtn = document.querySelector(".sign-in");
-
-document.querySelector(".signin").addEventListener("click", function () {
-  location.href = "./SignIn/signIn.html";
-});
-sign_inbtn.addEventListener("click", function () {
-  location.href = "./SignIn/signIn.html";
-});
-
-const user = document.querySelector(".user-profile");
-
-user.addEventListener("click", () => {
-  location.href = "./user_profile/userprofile.html";
-});
-document.querySelector(".profile").addEventListener("click", () => {
-  location.href = "./user_profile/userprofile.html";
-});
-const url = `https://cdn.dribbble.com/users/1516344/screenshots/6617658/17_liquidbackgrounds_2_1600x1200_1_4x.jpg`;
-setTimeout(() => {
-  document.body.style.backgroundImage = `url(${url})`;
-}, 3500);
-
-document.querySelector(".menu").addEventListener("click", () => {
-  document.querySelector(".overlay").style.display = "none";
-});
-const overlay = document.querySelector(".overlay");
-document.querySelector(".dropdown").addEventListener("click", () => {
-  overlay.style.display = "block";
-});
-
-const event = document.querySelector(".btn-events");
-event.addEventListener("click", () => {
-  location.href = "./Events/index.html";
-});
+AOS.init();
