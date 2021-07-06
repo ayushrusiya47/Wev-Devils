1ccc35e34b7d2eb1ad5179c07365751b20371bdc
Shikhar <75368010+shikhar13012001@users.noreply.github.com>
2021-07-06 14:12:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,18 +8,44 @@ window.addEventListener("resize", resize_blur);
 
 window.addEventListener("load", () => {
   if (token) {
-    console.log("test1"); //@shimkhar case: user signed in, make changes accordingly like remove signin, signup butttons add signout etc.
+    console.log("test1"); //!@shimkhar case: user signed in, make changes accordingly like remove signin, signup butttons add signout etc.
+    document.querySelector(".n-1").style.display="none";
+    document.querySelector(".f-2").style.display="none";
+    document.querySelector(".f-1").style.display="none";
+    document.querySelector(".n-2").style.display="none";
+    document.querySelector(".f-5").style.display="block";
+    document.querySelector(".n-4").style.display="block";
+    document.querySelector(".f-3").style.display="block";
+    document.querySelector(".n-3").style.display="block";
+     
+    
   } else {
-    console.log("test2"); //@shimkhar case: user not signed in, make changes accordingly
+    console.log("test2"); //!@shimkhar case: user not signed in, make changes accordingly
+    document.querySelector(".n-1").style.display="block";
+    document.querySelector(".f-2").style.display="block";
+    document.querySelector(".f-1").style.display="block";
+    document.querySelector(".n-2").style.display="block";
+    document.querySelector(".n-4").style.display="none";
+    document.querySelector(".f-5").style.display="none";
+    document.querySelector(".n-3").style.display="none";
+    document.querySelector(".f-3").style.display="none";
+
+  
   }
 }); //BackUpdate 1, For session management
+ 
 
-// var logout = ;
-// logout.addEventListener("click", () => {
-//   localStorage.setItem("jwt", "");
-//   location.reload();
-// }); //BackUpdate 2, for logout
-//@shimkhar create logout button and put it in var logout and uncomment
+let logout = document.querySelector(".logout");
+logout.addEventListener("click", () => {
+  localStorage.setItem("jwt", "");
+  location.reload();
+}); 
+let log_out = document.querySelector(".log-out");
+log_out.addEventListener("click", () => {
+  localStorage.setItem("jwt", "");
+  location.reload();
+});//BackUpdate 2, for logout
+//!@shimkhar create logout button and put it in var logout and uncomment
 
 function resize_blur() {
   body.style.filter = "blur(4px)";
